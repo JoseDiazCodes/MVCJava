d0c943fb597cb868004b82ce7017e19ec1528d77
JoseDiazCodes <josediazjr.a@gmail.com>
2024-11-16 18:54:59 -0500

    lab7

--- test/TicTacToeModelTest.java
+++ test/TicTacToeModelTest.java
@@ -1,10 +1,11 @@
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
 
 import tictactoe.Player;
 import tictactoe.TicTacToe;
